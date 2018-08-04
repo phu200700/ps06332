8abb3f720992062d0175b19bed18160f8517c632
QuangPhu <QuangPhu@WIN-J3EPQQPIG8E.localdomain>
2018-08-04 08:16:16 +0700

    aaaa

---
+++ src/lab6_ps06332/Lab6_ps06332.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab6_ps06332;
+
+/**
+ *
+ * @author QuangPhu
+ */
+public class Lab6_ps06332 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello");
+    }
+    
+}
